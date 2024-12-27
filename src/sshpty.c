377aa8e66bd941128662b2296b3427108b8d97eb
YGSRY <50430250+YGSRY@users.noreply.github.com>
2024-12-28 00:11:02 +0800

    Update sshpty.c

--- src/sshpty.c
+++ src/sshpty.c
@@ -3,7 +3,16 @@
 #include "errno.h"
 #include "sshpty.h"
 
-#if defined(USE_DEV_PTMX)
+/* Android 14兼容性处理，可能需要移除或替换一些系统调用 */
+
+#if defined(HAVE_OPENPTY)
+#undef HAVE_DEV_PTMX
+#endif
+
+#ifdef HAVE_PTY_H
+# include <pty.h>
+#endif
+#if defined(USE_DEV_PTMX) && defined(HAVE_STROPTS_H)
 # include <stropts.h>
 #endif
 
@@ -11,146 +20,156 @@
 #define O_NOCTTY 0
 #endif
 
-/* 分配伪终端 */
-int pty_allocate(int *ptyfd, int *ttyfd, char *namebuf, int namebuflen) {
+/*
+ * 为Android 14修改，支持通过设备管理分配PTY
+ * 返回0表示无法分配，返回非零表示成功。成功时返回PTY和TTY的文件描述符，和TTY名字。
+ */
+int
+pty_allocate(int *ptyfd, int *ttyfd, char *namebuf, int namebuflen)
+{
+#if defined(HAVE_OPENPTY)
+    /* 对于OpenPTY的兼容性处理：OpenPTY在Android 14上不直接支持，可能需要手动分配PTY */
+    char *name;
+    int i;
+
+    i = openpty(ptyfd, ttyfd, NULL, NULL, NULL);
+    if (i < 0) {
+        dropbear_log(LOG_WARNING, 
+                "pty_allocate: openpty: %.100s", strerror(errno));
+        return 0;
+    }
+    name = ttyname(*ttyfd);
+    if (!name) {
+        dropbear_exit("ttyname fails for openpty device");
+    }
+
+    strlcpy(namebuf, name, namebuflen); /* 可能会截断 */
+    return 1;
+#else /* HAVE_OPENPTY */
+    #if defined(USE_DEV_PTMX)
+    /* Android 14可能使用/dev/ptmx设备 */
     int ptm;
     char *pts;
 
-    /* 打开主伪终端设备 */
     ptm = open("/dev/ptmx", O_RDWR | O_NOCTTY);
     if (ptm < 0) {
-        dropbear_log(LOG_WARNING, "pty_allocate: /dev/ptmx: %.100s", strerror(errno));
+        dropbear_log(LOG_WARNING,
+                "pty_allocate: /dev/ptmx: %.100s", strerror(errno));
         return 0;
     }
 
-    /* 授权并解锁伪终端 */
     if (grantpt(ptm) < 0) {
-        dropbear_log(LOG_WARNING, "grantpt: %.100s", strerror(errno));
-        return 0;
-    }
-    if (unlockpt(ptm) < 0) {
-        dropbear_log(LOG_WARNING, "unlockpt: %.100s", strerror(errno));
+        dropbear_log(LOG_WARNING,
+                "grantpt: %.100s", strerror(errno));
         return 0;
     }
 
-    /* 获取伪终端的名字 */
+    if (unlockpt(ptm) < 0) {
+        dropbear_log(LOG_WARNING,
+                "unlockpt: %.100s", strerror(errno));
+        return 0;
+    }
+
     pts = ptsname(ptm);
     if (pts == NULL) {
-        dropbear_log(LOG_WARNING, "Slave pty side name could not be obtained.");
-        return 0;
+        dropbear_log(LOG_WARNING,
+                "Slave pty side name could not be obtained.");
     }
 
     strlcpy(namebuf, pts, namebuflen);
     *ptyfd = ptm;
 
-    /* 打开从伪终端 */
+    /* 打开从设备 */
     *ttyfd = open(namebuf, O_RDWR | O_NOCTTY);
     if (*ttyfd < 0) {
-        dropbear_log(LOG_ERR, "error opening pts %.100s: %.100s", namebuf, strerror(errno));
+        dropbear_log(LOG_ERR,
+            "error opening pts %.100s: %.100s", namebuf, strerror(errno));
         close(*ptyfd);
         return 0;
     }
 
-    /* 如果系统支持，推送流模块 */
-    #if !defined(HAVE_CYGWIN) && defined(I_PUSH)
-        if (ioctl(*ttyfd, I_PUSH, "ptem") < 0) {
-            dropbear_log(LOG_WARNING, "ioctl I_PUSH ptem: %.100s", strerror(errno));
-        }
-        if (ioctl(*ttyfd, I_PUSH, "ldterm") < 0) {
-            dropbear_log(LOG_WARNING, "ioctl I_PUSH ldterm: %.100s", strerror(errno));
-        }
-        #ifndef __hpux
-        if (ioctl(*ttyfd, I_PUSH, "ttcompat") < 0) {
-            dropbear_log(LOG_WARNING, "ioctl I_PUSH ttcompat: %.100s", strerror(errno));
-        }
-        #endif
-    #endif
-
     return 1;
+    #else /* USE_DEV_PTMX */
+    /* 通过设备路径分配 */
+    const char *name;
+
+    *ptyfd = open("/dev/ptc", O_RDWR | O_NOCTTY);
+    if (*ptyfd < 0) {
+        dropbear_log(LOG_ERR,
+            "Could not open /dev/ptc: %.100s", strerror(errno));
+        return 0;
+    }
+    name = ttyname(*ptyfd);
+    if (!name) {
+        dropbear_exit("ttyname fails for /dev/ptc device");
+    }
+    strlcpy(namebuf, name, namebuflen);
+    *ttyfd = open(name, O_RDWR | O_NOCTTY);
+    if (*ttyfd < 0) {
+        dropbear_log(LOG_ERR,
+            "Could not open pty slave side %.100s: %.100s",
+            name, strerror(errno));
+        close(*ptyfd);
+        return 0;
+    }
+    return 1;
+    #endif /* USE_DEV_PTMX */
+#endif /* HAVE_OPENPTY */
 }
 
-/* 释放伪终端 */
-void pty_release(const char *tty_name) {
-    if (chown(tty_name, (uid_t) 0, (gid_t) 0) < 0 && (errno != ENOENT)) {
-        dropbear_log(LOG_ERR, "chown %.100s 0 0 failed: %.100s", tty_name, strerror(errno));
+/* 释放TTY，恢复其归属 */
+void
+pty_release(const char *tty_name)
+{
+    if (chown(tty_name, (uid_t) 0, (gid_t) 0) < 0
+            && (errno != ENOENT)) {
+        dropbear_log(LOG_ERR,
+                "chown %.100s 0 0 failed: %.100s", tty_name, strerror(errno));
     }
-    if (chmod(tty_name, (mode_t) 0666) < 0 && (errno != ENOENT)) {
-        dropbear_log(LOG_ERR, "chmod %.100s 0666 failed: %.100s", tty_name, strerror(errno));
+    if (chmod(tty_name, (mode_t) 0666) < 0
+            && (errno != ENOENT)) {
+        dropbear_log(LOG_ERR,
+            "chmod %.100s 0666 failed: %.100s", tty_name, strerror(errno));
     }
 }
 
-/* 设置控制终端 */
-void pty_make_controlling_tty(int *ttyfd, const char *tty_name) {
+/* 设置TTY为当前进程的控制TTY */
+void
+pty_make_controlling_tty(int *ttyfd, const char *tty_name)
+{
     int fd;
-#ifdef USE_VHANGUP
-    void *old;
-#endif /* USE_VHANGUP */
 
-    /* 忽略SIGTTOU信号 */
+    /* Android上可能不支持TIOCNOTTY，因此可以跳过 */
     signal(SIGTTOU, SIG_IGN);
 
-    /* 首先断开与旧的控制终端 */
-#ifdef TIOCNOTTY
-    fd = open(_PATH_TTY, O_RDWR | O_NOCTTY);
-    if (fd >= 0) {
-        (void) ioctl(fd, TIOCNOTTY, NULL);
-        close(fd);
-    }
-#endif /* TIOCNOTTY */
-
-    /* 创建新会话 */
+    /* 首先断开当前控制TTY */
     if (setsid() < 0) {
-        dropbear_log(LOG_ERR, "setsid: %.100s", strerror(errno));
+        dropbear_log(LOG_ERR,
+            "setsid: %.100s", strerror(errno));
     }
 
-    /* 验证我们是否成功断开控制终端 */
-    fd = open(_PATH_TTY, O_RDWR | O_NOCTTY);
-    if (fd >= 0) {
-        dropbear_log(LOG_ERR, "Failed to disconnect from controlling tty.");
-        close(fd);
-    }
-
-    /* 设置新的控制终端 */
+    /* 设置控制TTY */
 #ifdef TIOCSCTTY
     if (ioctl(*ttyfd, TIOCSCTTY, NULL) < 0) {
-        dropbear_log(LOG_ERR, "ioctl(TIOCSCTTY): %.100s", strerror(errno));
+        dropbear_log(LOG_ERR,
+            "ioctl(TIOCSCTTY): %.100s", strerror(errno));
     }
-#endif /* TIOCSCTTY */
-#ifdef HAVE_NEWS4
-    if (setpgrp(0, 0) < 0) {
-        dropbear_log(LOG_ERR, "setpgrp: %.100s", strerror(errno));
-    }
-#endif /* HAVE_NEWS4 */
-#ifdef USE_VHANGUP
-    old = mysignal(SIGHUP, SIG_IGN);
-    vhangup();
-    mysignal(SIGHUP, old);
-#endif /* USE_VHANGUP */
+#endif
 
-    /* 打开指定的tty设备 */
     fd = open(tty_name, O_RDWR);
     if (fd < 0) {
-        dropbear_log(LOG_ERR, "%.100s: %.100s", tty_name, strerror(errno));
-    } else {
-#ifdef USE_VHANGUP
-        close(*ttyfd);
-        *ttyfd = fd;
-#else /* USE_VHANGUP */
-        close(fd);
-#endif /* USE_VHANGUP */
-    }
-
-    /* 验证是否已成功设置控制终端 */
-    fd = open(_PATH_TTY, O_WRONLY);
-    if (fd < 0) {
-        dropbear_log(LOG_ERR, "open /dev/tty failed - could not set controlling tty: %.100s", strerror(errno));
+        dropbear_log(LOG_ERR,
+            "%.100s: %.100s", tty_name, strerror(errno));
     } else {
         close(fd);
     }
 }
 
-/* 更改伪终端窗口大小 */
-void pty_change_window_size(int ptyfd, int row, int col, int xpixel, int ypixel) {
+/* 修改窗口大小 */
+void
+pty_change_window_size(int ptyfd, int row, int col,
+    int xpixel, int ypixel)
+{
     struct winsize w;
 
     w.ws_row = row;
@@ -160,13 +179,15 @@ void pty_change_window_size(int ptyfd, int row, int col, int xpixel, int ypixel)
     (void) ioctl(ptyfd, TIOCSWINSZ, &w);
 }
 
-void pty_setowner(struct passwd *pw, const char *tty_name) {
+void
+pty_setowner(struct passwd *pw, const char *tty_name)
+{
     struct group *grp;
     gid_t gid;
     mode_t mode;
     struct stat st;
 
-    /* 获取“tty”组 */
+    /* 获取tty的组信息 */
     grp = getgrnam("tty");
     if (grp) {
         gid = grp->gr_gid;
@@ -176,29 +197,24 @@ void pty_setowner(struct passwd *pw, const char *tty_name) {
         mode = S_IRUSR | S_IWUSR | S_IWGRP | S_IWOTH;
     }
 
-    /* 获取tty设备信息 */
+    /* 修改所有者和权限 */
     if (stat(tty_name, &st)) {
-        dropbear_exit("pty_setowner: stat(%.101s) failed: %.100s", tty_name, strerror(errno));
+        dropbear_exit("pty_setowner: stat(%.101s) failed: %.100s",
+                tty_name, strerror(errno));
     }
 
-    /* 修改tty的拥有者和权限 */
     if (st.st_uid != pw->pw_uid || !(st.st_gid == gid || st.st_gid == pw->pw_gid)) {
         if (chown(tty_name, pw->pw_uid, gid) < 0) {
-            if (errno == EROFS && (st.st_uid == pw->pw_uid || st.st_uid == 0)) {
-                dropbear_log(LOG_ERR, "chown(%.100s, %u, %u) failed: %.100s", tty_name, (unsigned int)pw->pw_uid, (unsigned int)gid, strerror(errno));
-            } else {
-                dropbear_exit("chown(%.100s, %u, %u) failed: %.100s", tty_name, (unsigned int)pw->pw_uid, (unsigned int)gid, strerror(errno));
-            }
+            dropbear_exit("chown(%.100s, %u, %u) failed: %.100s",
+                tty_name, (unsigned int)pw->pw_uid, (unsigned int)gid,
+                strerror(errno));
         }
     }
 
-    if ((st.st_mode & (S_IRWXU | S_IRWXG | S_IRWXO)) != mode) {
+    if ((st.st_mode & (S_IRWXU|S_IRWXG|S_IRWXO)) != mode) {
         if (chmod(tty_name, mode) < 0) {
-            if (errno == EROFS && (st.st_mode & (S_IRGRP | S_IROTH)) == 0) {
-                dropbear_log(LOG_ERR, "chmod(%.100s, 0%o) failed: %.100s", tty_name, mode, strerror(errno));
-            } else {
-                dropbear_exit("chmod(%.100s, 0%o) failed: %.100s", tty_name, mode, strerror(errno));
-            }
+            dropbear_exit("chmod(%.100s, 0%o) failed: %.100s",
+                tty_name, mode, strerror(errno));
         }
     }
 }
